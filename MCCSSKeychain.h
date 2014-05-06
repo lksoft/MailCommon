fc541b78599b4f35c5e66b06948874a54588454d
Scott Little <scott@littleknownsoftware.com>
2014-05-06 15:07:36 +0200

    Add a prefixing header for SSKeychain!

---
+++ MCCSSKeychain.h
@@ -0,0 +1,29 @@
+//
+//  MCCSSKeychain.h
+//  OAuth Tester
+//
+//  Created by Scott Little on 6/5/14.
+//  Copyright (c) 2014 Little Known Software, Inc. All rights reserved.
+//
+
+#include "MCCCommonHeader.h"
+
+#define	SSKeychain								MCC_PREFIXED_NAME(Keychain)
+#define	SSKeychainQuery							MCC_PREFIXED_NAME(KeychainQuery)
+
+#define	SSKeychainErrorCode						MCC_PREFIXED_NAME(KeychainErrorCode)
+#define	SSKeychainErrorBadArguments				MCC_PREFIXED_NAME(KeychainErrorBadArguments)
+
+#define	kSSKeychainErrorDomain					MCC_PREFIXED_CONSTANT(KeychainErrorDomain)
+#define	kSSKeychainAccountKey					MCC_PREFIXED_CONSTANT(KeychainAccountKey)
+#define	kSSKeychainCreatedAtKey					MCC_PREFIXED_CONSTANT(KeychainCreatedAtKey)
+#define	kSSKeychainClassKey						MCC_PREFIXED_CONSTANT(KeychainClassKey)
+#define	kSSKeychainDescriptionKey				MCC_PREFIXED_CONSTANT(KeychainDescriptionKey)
+#define	kSSKeychainLabelKey						MCC_PREFIXED_CONSTANT(KeychainLabelKey)
+#define	kSSKeychainLastModifiedKey				MCC_PREFIXED_CONSTANT(KeychainLastModifiedKey)
+#define	kSSKeychainWhereKey						MCC_PREFIXED_CONSTANT(KeychainWhereKey)
+
+#define	SSKeychainQuerySynchronizationMode		MCC_PREFIXED_NAME(KeychainQuerySynchronizationMode)
+#define	SSKeychainQuerySynchronizationModeAny	MCC_PREFIXED_NAME(KeychainQuerySynchronizationModeAny)
+#define	SSKeychainQuerySynchronizationModeNo	MCC_PREFIXED_NAME(KeychainQuerySynchronizationModeNo)
+#define	SSKeychainQuerySynchronizationModeYes	MCC_PREFIXED_NAME(KeychainQuerySynchronizationModeYes)
