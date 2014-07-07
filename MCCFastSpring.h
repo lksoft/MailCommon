406bf7289974aba55568d1fd91433f1b7d217ee5
Scott Little <scott@littleknownsoftware.com>
2014-07-07 22:15:40 +0200

    Add FastSpring Embedded Store Common Header.

---
+++ MCCFastSpring.h
@@ -0,0 +1,56 @@
+//
+//  MCCFastSpring.h
+//  MailCommon
+//
+//  Created by Scott Little on 7/7/14.
+//  Copyright (c) 2014 Little Known Software. All rights reserved.
+//
+
+#import "MCCCommonHeader.h"
+
+
+#pragma mark - Fsprg Mappings
+
+//	From FsprgEmbeddedStoreController.h
+#define	FsprgEmbeddedStoreController	MCC_PREFIXED_NAME(FSStoreController)
+
+//	From FsprgEmbeddedStoreDelegate
+#define	FsprgEmbeddedStoreDelegate		MCC_PREFIXED_NAME(FSStoreDelegate)
+
+//	Other Symbols
+#define	FsprgPageType					MCC_PREFIXED_NAME(FastSpringPageType)
+#define	FsprgPageFS						MCC_PREFIXED_NAME(FastSpringPageTypeFS)
+#define	FsprgPagePayPal					MCC_PREFIXED_NAME(FastSpringPageTypePayPal)
+#define	FsprgPageUnknown				MCC_PREFIXED_NAME(FastSpringPageTypeUnknown)
+
+
+//	From FsprgOrderView
+#define	FsprgOrderView					MCC_PREFIXED_NAME(FSOrderView)
+
+//	From FsprgStoreParameters
+#define	FsprgStoreParameters			MCC_PREFIXED_NAME(FSStoreParameters)
+
+//	Other Symbols
+#define	kFsprgOrderProcessDetail		MCC_PREFIXED_CONSTANT(FastSpringOrderProcessDetail)
+#define	kFsprgOrderProcessInstant		MCC_PREFIXED_CONSTANT(FastSpringOrderProcessInstant)
+#define	kFsprgOrderProcessCheckout		MCC_PREFIXED_CONSTANT(FastSpringOrderProcessCheckout)
+
+#define	kFsprgModeActive				MCC_PREFIXED_CONSTANT(FastSpringModeActive)
+#define	kFsprgModeActiveTest			MCC_PREFIXED_CONSTANT(FastSpringModeActiveTest)
+#define	kFsprgModeTest					MCC_PREFIXED_CONSTANT(FastSpringModeTest)
+
+
+//	From FsprgOrder
+#define	FsprgOrder						MCC_PREFIXED_NAME(FSOrder)
+
+//	From FsprgOrderItem
+#define	FsprgOrderItem					MCC_PREFIXED_NAME(FSOrderItem)
+
+//	From FsprgFileDownload
+#define	FsprgFileDownload				MCC_PREFIXED_NAME(FSFileDownload)
+
+//	From FsprgFulfillment
+#define	FsprgFulfillment				MCC_PREFIXED_NAME(FSFulfillment)
+
+//	From FsprgLicense
+#define	FsprgLicense					MCC_PREFIXED_NAME(FSLicense)
