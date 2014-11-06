0cd8c24ed0cc8abc3072e0b807b8f739d59cc51a
Scott Little <scott@littleknownsoftware.com>
2014-11-06 11:54:48 +0100

    Add MGTemplateEngine mappers.

---
+++ MCCTemplateEngine.h
@@ -0,0 +1,37 @@
+//
+//  MCCTemplateEngine.h
+//  MailCommon
+//
+//  Created by Scott Little on 06/11/14.
+//  Copyright (c) 2014 Little Known Software. All rights reserved.
+//
+//
+
+#import "MCCCommonHeader.h"
+
+
+#pragma mark - MGTemplateEngine Mappings
+
+//	From MGTemplateEngine
+#define	MGTemplateEngine			MCC_PREFIXED_NAME(TemplateEngine)
+#define	MGTemplateEngineDelegate	MCC_PREFIXED_NAME(TemplateEngineDelegate)
+#define	MGTemplateEngineMatcher		MCC_PREFIXED_NAME(TemplateEngineMatcher)
+
+//	From MGTemplateMarker
+#define	MGTemplateMarker			MCC_PREFIXED_NAME(TemplateMarker)
+
+//	From MGTemplateFilter
+#define	MGTemplateFilter			MCC_PREFIXED_NAME(TemplateFilter)
+
+//	From MGTemplateStandardMarkers
+#define	MGTemplateStandardMarkers	MCC_PREFIXED_NAME(TemplateStandardMarkers)
+
+//	From MGTemplateStandardFilters
+#define	MGTemplateStandardFilters	MCC_PREFIXED_NAME(TemplateStandardFilters)
+
+//	From ICUTemplateMatcher
+#define	ICUTemplateMatcher			MCC_PREFIXED_NAME(TemplateMatcher)
+
+//	From DeepMutableCopy
+#define	deepMutableCopy				MCC_PREFIXED_NAME(deepMutableCopy)
+
